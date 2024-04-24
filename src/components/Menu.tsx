7af0bac501e7bdbd57eb69bc04983432425947d4
roman <roman@MacBook-Air-roman.local>
2024-04-24 23:53:13 +0300

    feat: added logic for painting the active menu link

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,16 +1,24 @@
-'use client'
+"use client";
 
 import { HeaderLinks } from "@/constants";
 import { LanguageContext } from "@/context";
+import { cn } from "@/utils";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { useContext } from "react";
 
 export const Menu = () => {
   const { language } = useContext(LanguageContext);
+  const pathname = usePathname();
   return (
     <div className="flex gap-8">
       {Object.values(HeaderLinks[language]).map((link) => (
-        <Link href={link.href} className="transition hover:opacity-60">
+        <Link
+          href={link.href}
+          className={cn("transition hover:opacity-60", {
+            "text-slate-400 border-b border-slate-400": pathname === link.href,
+          })}
+        >
           {link.title}
         </Link>
       ))}
