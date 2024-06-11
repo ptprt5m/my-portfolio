a244e182d32a99e6f5928a24820d9807852a01d4
roman <roman@MacBook-Air-roman.local>
2024-06-11 10:47:52 +0300

    fix: about const refactoring

--- src/constants/about.ts
+++ src/constants/about.ts
@@ -8,42 +8,28 @@ export const About = {
         "Привет! Меня зовут Роман, и я фронтенд-разработчик.",
         "Меня всегда привлекала возможность создавать интерактивные и визуально привлекательные веб-сайты, которые обеспечивают отличный пользовательский опыт.",
       ],
-      education: [
-        "Я окончил Сыктывкарский Государственный Университет им. Питирима Сорокина по направлению Прикладная Информатика в экономике.",
-        "Также я проходил несколько небольших курсов по фронтенд-разработке на ютубе.",
+      professionalExperience: [
+        "В процессе работы я сталкивался с широким спектром технических задач: от оптимизации производительности в соответствии с метриками Google PageSpeed Insights до интеграции сложных API и разработки архитектуры веб-приложений.",
+        "Мой опыт позволяет мне находить эффективные решения для повышения скорости загрузки и улучшения взаимодействия пользователя с сайтом.",
       ],
-      personalQualities: [
-        "Я обладаю рядом личных качеств, которые, как я считаю, делают меня хорошим разработчиком.",
-        "Я всегда уделяю внимание деталям, умею работать в команде и способен решать сложные задачи.",
-        "Мне нравится постоянно учиться и развиваться в своей области.",
-      ],
-      interests: [
-        "Вне работы у меня есть несколько интересов.",
-        "Я люблю заниматься спортом. Он помогает мне расслабиться и вдохновляет на новые идеи.",
-        "Также я увлекаюсь чтением, игрой в шахматы и боксированием, что помогает мне сохранять баланс между работой и личной жизнью.",
+      developmentPhilosophy: [
+        "Моя работа над ключевыми проектами включала создание структурированных и модульных систем, обеспечивающих гибкость и масштабируемость. Я придерживаюсь принципов чистого кода и рефакторинга для поддержания высокого качества продукта и упрощения последующей поддержки и развития.",
       ],
     },
   },
   [LANGUAGES.EN]: {
-    title: "About me",
+    title: "About Me",
     items: {
       introduction: [
         "Hello! My name is Roman, and I am a frontend developer.",
-        "I have always been attracted to the opportunity to create interactive and visually appealing websites that provide an excellent user experience.",
+        "I have always been drawn to the opportunity to create interactive and visually appealing websites that provide an excellent user experience.",
       ],
-      education: [
-        "I graduated from Syktyvkar State University named after Pitirim Sorokin with a degree in Applied Informatics in Economics.",
-        "I have also completed several small courses on frontend development on YouTube.",
+      professionalExperience: [
+        "Throughout my career, I have encountered a wide range of technical challenges, from optimizing performance according to Google PageSpeed Insights metrics to integrating complex APIs and developing web application architectures.",
+        "My experience allows me to find effective solutions that enhance site loading speed and improve user interaction.",
       ],
-      personalQualities: [
-        "I possess a number of personal qualities that, I believe, make me a good developer.",
-        "I always pay attention to details, can work in a team, and am capable of solving complex tasks.",
-        "I like to constantly learn and develop in my field.",
-      ],
-      interests: [
-        "Outside of work, I have several interests.",
-        "I love doing sports. It helps me relax and inspires new ideas.",
-        "I am also fond of reading, playing chess, and boxing, which helps me maintain a balance between work and personal life.",
+      developmentPhilosophy: [
+        "My work on key projects has involved creating structured and modular systems that ensure flexibility and scalability. I adhere to the principles of clean code and refactoring to maintain high product quality and simplify ongoing support and development.",
       ],
     },
   },
